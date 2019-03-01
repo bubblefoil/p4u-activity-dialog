9949960b24ca2913cd341b29bd04307fc686b52c
bubblefoil <go.holyal@gmail.com>
2019-03-01 14:37:21 +0100

    Task table sort menu opens by left-click

--- p4u-dialog.user.js
+++ p4u-dialog.user.js
@@ -1,26 +1,49 @@
 // ==UserScript==
 // @name         p4u-activity-dialog
 // @description  Keyboard shortcuts for P4U Activity dialogs
-// @version      1.0.0
+// @version      1.1.0
 // @namespace    https://plus4u.net/
 // @author       bubblefoil
 // @license      MIT
 // @match        https://plus4u.net/*
 // @run-at       document-idle
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function () {
     'use strict';
-    var onFrameLoaded = function (iframe) {
+
+    GM_addStyle(`
+    .p4u-ext-clickable:hover{
+        cursor: pointer;    
+    }
+     `);
+
+    function enhanceTable() {
+        const th1 = document.getElementById('listID_headcell_0');
+        if (!th1) return;
+
+        Array
+            .from(th1.parentElement.cells)
+            .slice(1, 5)//drop first and last cols
+            .forEach(c => {
+                c.classList.add('p4u-ext-clickable');
+                c.onclick = function (e) {
+                    c.oncontextmenu(e);
+                };
+            });
+    }
+
+    const onFrameLoaded = function (iframe) {
         if (iframe.id !== 'modal-iframe1') {
-            console.log('p4u-activity-dialog:',`No iframe with id "modal-iframe1"`);
+            console.debug('p4u-activity-dialog:', `No iframe with id "modal-iframe1"`);
             return;
         }
-        var btnOk = iframe.contentDocument.getElementById('form-btn-ok');
+        const btnOk = iframe.contentDocument.getElementById('form-btn-ok');
         if (btnOk) {
             btnOk.parentElement.title = "Ctrl + Enter";
         }
-        var btnCancel = iframe.contentDocument.getElementById('form-btn-cancel');
+        const btnCancel = iframe.contentDocument.getElementById('form-btn-cancel');
         if (btnCancel) {
             btnCancel.parentElement.title = "Escape";
         }
@@ -31,7 +54,7 @@
                 btnCancel.parentElement.click();
             }
         };
-        var optionsBlock = iframe.contentDocument.getElementById('listbox_FIRST_ListContentTag');
+        const optionsBlock = iframe.contentDocument.getElementById('listbox_FIRST_ListContentTag');
         if (optionsBlock) {
             setTimeout(function () {
                 optionsBlock.focus();
@@ -39,17 +62,19 @@
         }
     };
 
-    var observer = new MutationObserver(function (mutations) {
+    const observer = new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
             [].filter.call(mutation.addedNodes, function (node) {
                 return node.nodeName === 'IFRAME' && node.id === 'modal-iframe1';
             }).forEach(function (node) {
                 node.addEventListener('load', function (e) {
-                    console.log('p4u-activity-dialog:', 'enhancing loaded iframe', node.id);
+                    console.debug('p4u-activity-dialog:', 'enhancing loaded iframe', node.id);
                     onFrameLoaded(node);
                 });
             });
         });
     });
+
     observer.observe(document.body, {childList: true, subtree: true});
+    enhanceTable();
 })();
